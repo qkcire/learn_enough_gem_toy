345b84364e17bc6f3be8b81ea7416d78ce91b105
Erick Quintanilla <equintanilla@berkeley.edu>
2020-04-29 07:29:07 -0700

    Initialize gem repo

---
+++ lib/equintanilla_palindrome.rb
@@ -0,0 +1,6 @@
+require "equintanilla_palindrome/version"
+
+module EquintanillaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/equintanilla_palindrome/version.rb
@@ -0,0 +1,3 @@
+module EquintanillaPalindrome
+  VERSION = "0.1.0"
+end
